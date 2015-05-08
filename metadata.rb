65ae7573da843348ed8ad1f15e524dbdb4aa8b5f
Edmund Haselwanter <me@ehaselwanter.com>
2015-05-08 11:45:09 +0200

    add new dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,15 @@
-maintainer          "Jay Pipes"
-maintainer_email    "jaypipes@gmail.com"
-license             "Apache 2.0"
-description         "Installs the Zuul Gerrit/Jenkins job management service"
-long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "0.2.0"
+name "zuul"
+maintainer 'Edmund Haselwanter'
+maintainer_email 'team@infralovers.com'
+license 'Apache 2.0'
+description 'Installs the Zuul Gerrit/Jenkins job management service'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.3.0'
 
-%w{ fedora redhat centos ubuntu debian amazon }.each do |os|
+%w(ubuntu).each do |os|
   supports os
 end
 
-depends             "jenkins"
-# Not currently any good Gerrit cookbook :(
-# depends             "gerrit"
+depends 'python'
+depends 'git'
+depends 'build-essential'
