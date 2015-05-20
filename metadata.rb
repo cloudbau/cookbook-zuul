ff3af6c48019d02cf18860a0313970f94764da74
Edmund Haselwanter <me@ehaselwanter.com>
2015-05-20 09:49:46 +0200

    update patch version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'team@infralovers.com'
 license 'Apache 2.0'
 description 'Installs the Zuul Gerrit/Jenkins job management service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.0'
+version '0.3.1'
 
 %w(ubuntu).each do |os|
   supports os
